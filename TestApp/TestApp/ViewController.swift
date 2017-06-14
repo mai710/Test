5c5d8a7cb347b51afc90cdae44ac283ab16ae4b5
Mai EL-Awini <mai.elawini@gmail.com>
2017-06-14 11:17:37 +0400

    test app

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Mai El Awini on 6/14/17.
+//  Copyright © 2017 Advance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
